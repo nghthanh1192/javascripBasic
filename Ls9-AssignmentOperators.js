8739f061ac0a833d38e448e1f0cc68bfaa6201b3
Thanh Nguyen <hthanh1192@gmail.com>
2019-09-24 23:01:08 +0700

    Ls9 Assignment Operators

---
+++ Ls9-AssignmentOperators.js
@@ -0,0 +1,7 @@
+// 4. = += -= *= /=
+
+var a = 5;
+// a += 1;
+// console.log(a);
+a *= 2;
+console.log(a);
---
+++ Ls9-Ex2.js
@@ -0,0 +1,10 @@
+var a = 10;
+
+/**
+ * Viết ra tất cả các cách khác nhau có thể (trong phạm vi bài học) để gấp đôi a
+ */
+
+var a1 = a * 2;
+var a2 = a + a;
+var a3;
+a3 *= a;
