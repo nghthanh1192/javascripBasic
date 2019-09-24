48a7c1db6b624c6802b8cf5251b861794be1af8b
Thanh Nguyen <hthanh1192@gmail.com>
2019-09-24 22:00:33 +0700

    Ls7 Arithmetic Operators

---
+++ Ls7-ArithmeticOperators.js
@@ -0,0 +1,13 @@
+// Arithmetic Operators
+// Thứ tự Các phép toán học trong lập trình
+// 1. ++ --
+// 2. * / %
+// 3. + -
+
+// 4 - 6 / 3 % 4
+// 4 - 2 % 4
+// 4 - 2
+// 2
+
+var x = 4 - 6 / 3 % 4;
+console.log(x);
