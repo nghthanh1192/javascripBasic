d69c26c5244f110b240c2d50a420aaffc9e52e1e
Thanh Nguyen <hthanh1192@gmail.com>
2019-09-24 23:14:52 +0700

    Ls10 Function

---
+++ Ls10-Function.js
@@ -0,0 +1,19 @@
+// Define a function
+// function doSomething(input1, input2, ...) {
+//     // code
+//     return something;
+// }
+
+// Keywords: functionk return
+// Function name: doSomething
+// Parameter names: input1, input2, ...
+
+// Execute/Call a Function
+// doSomething(input1, input2, ...)
+
+function calculateTriangleSquare(a, h) {
+    return a * h /2;
+}
+
+// Tính điện tích a = 10, b = 5
+console.log(calculateTriangleSquare(10, 5));
